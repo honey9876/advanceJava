2492821f1d4cba0f4cd04f76342f5f35ed4a6163
Honey sharma <114321260+honey9876@users.noreply.github.com>
2024-12-10 23:46:38 +0530

    updatecode

---
+++ lacture32/Greater.java
@@ -0,0 +1,19 @@
+package lacture32;
+
+import java.util.Stack;
+
+public class Greater {
+    public static void main(String[] args) {
+        int arr[] = {6, 8, 0, 1, 3};
+        Stack<Integer> s = new Stack<>();
+        int nxtGreater[] = new int [arr.length];
+
+        for(int i=arr.length-1; i>=0; i--){
+            while (!s.isEmpty() &&) {
+                
+            }
+        }
+
+    }
+    
+}
