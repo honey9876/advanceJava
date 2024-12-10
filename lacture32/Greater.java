6da7e7578cc90d310e656f83d6adea5e1fa7e057
Honey sharma <114321260+honey9876@users.noreply.github.com>
2024-12-11 00:01:22 +0530

    grater element

--- lacture32/Greater.java
+++ lacture32/Greater.java
@@ -9,11 +9,23 @@ public class Greater {
         int nxtGreater[] = new int [arr.length];
 
         for(int i=arr.length-1; i>=0; i--){
-            while (!s.isEmpty() &&) {
+            while(!s.isEmpty() && arr[s.peek()] <= arr[i]) {
+                s.pop();
+            }
+            if (s.isEmpty()) {
+                nxtGreater[i] = -1;
+            }else{
+                nxtGreater[i] = arr[ s.peek()];   
                 
             }
+            s.push(i);
         }
 
+        for(int i =0; i<nxtGreater.length; i++) {
+                System.out.print(nxtGreater[i]+" ");
+            
+            }
+            System.out.println(); 
     }
     
 }
