42a6ab2addb7886a1433ff05283b423ee4978bd9
Honey sharma <114321260+honey9876@users.noreply.github.com>
2024-12-16 17:54:50 +0530

    front imp

--- Queue/array.java
+++ Queue/array.java
@@ -7,25 +7,35 @@ public class array {
     static int arr[];
     static int size;
     static int rear;
+    static int front;
 
 
         Queue(int n){
             arr = new int[n];
             size = n;
             rear = -1; 
+            front = -1;
         }
 
         public static boolean isEmpty(){
-            return rear == -1;
+            return rear == -1 && front == -1;
+        }
+
+
+        public static boolean isFull() {
+            return (rear+1)% size == front;
         }
 
         public static void add(int data){
-            if (rear == size-1) {
+            if (isFull()) {
                 System.out.println("queue is full");
                 return;
             }
+            if (front == -1) {
+                 front = 0;
+            }
 
-            rear = rear + 1;
+            rear = (rear + 1) % size;
             arr[rear] = data;
 
         }
@@ -35,33 +45,44 @@ public class array {
                 return -1;
 
             }
-            int front = arr[0];
-            for(int i=0; i<rear; i++){
-                arr[i] = arr[i+1];
 
+            int result = arr[front];
+            
+
+            if (rear == front) {
+                rear = front = -1;
+                
+            } else {
+                front = (front +1)%size;
 
             }
-            rear = rear -1;
             return front;
 
         }
+
         public static int peek(){
             if (isEmpty()) {
                 System.out.println("empty queue");
                 return -1;
 
             }
-            return arr[0];
+            return arr[front];
         }
 
     
         
     }
     public static void main(String[] args) {
-        Queue q = new Queue(5);
+        Queue q = new Queue(3);
         q.add(1);
         q.add(2);
         q.add(3);
+        System.out.println(q.remove());
+          
+        q.add(4);
+        System.out.println(q.remove());
+        q.add(5);
+
 
         while (!q.isEmpty()) {
             System.out.println(q.peek());
