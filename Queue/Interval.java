eaba1c0ae9efc735e8e949ff2c34328cf71715c7
Honey sharma <114321260+honey9876@users.noreply.github.com>
2024-12-19 11:24:13 +0530

    interleave 2 half of a queue

--- Queue/Interval.java
+++ Queue/Interval.java
@@ -1,6 +1,46 @@
 package Queue;
+import java.util.*;
 
 public class Interval {
+    public static void interleave(Queue<Integer> q){
+        Queue<Integer> firstHwaf = new LinkedList<>();
+        int size = q.size();
+
+        for(int i=0; i<size/2; i++){
+           firstHwaf.add(q.remove()); 
+        }
+
+        while (!firstHwaf.isEmpty()) {
+            q.add(firstHwaf.remove());
+            q.add(q.remove());
+        }
+
+    }
+
+
+
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        q.add(6);
+        q.add(7);
+        q.add(8);
+        q.add(9);
+        q.add(10);
+
+        interleave(q);
+        while (!q.isEmpty()) {
+            System.out.print(q.remove() + " ");
+        }
+      System.out.println();
+
+    }
+
+
     
     
 }
