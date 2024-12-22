e3a45ddab1f8074ad29bf18f1dbc9cec0e52a50d
Honey sharma <114321260+honey9876@users.noreply.github.com>
2024-12-22 12:20:28 +0530

    Accept an integer and check whether it is an even number or odd

---
+++ dailyProblem/EvenOddChecke.java
@@ -0,0 +1,20 @@
+package dailyProblem;
+
+import java.util.Scanner;
+
+public class EvenOddChecke{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int number = scanner.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is an even number.");
+        } else {
+            System.out.println(number + " is an odd number.");
+        }
+
+        scanner.close();
+    }
+}
+
