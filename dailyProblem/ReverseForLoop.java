f4cca27641a8dea6ea20242cd430a1d8e5924eab
Honey sharma <114321260+honey9876@users.noreply.github.com>
2025-01-03 22:26:16 +0530

    Reverse for loop. Print n to 1.

---
+++ dailyProblem/ReverseForLoop.java
@@ -0,0 +1,11 @@
+package dailyProblem;
+
+
+public class ReverseForLoop {
+    public static void main(String[] args) {
+        int n = 10; 
+        for (int i = n; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+}
