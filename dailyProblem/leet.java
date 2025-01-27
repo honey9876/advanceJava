8a347d04115fa6b00542c72b959a3a9abc510d92
Honey sharma <114321260+honey9876@users.noreply.github.com>
2025-01-27 22:57:26 +0530

    update code

--- dailyProblem/leet.java
+++ dailyProblem/leet.java
@@ -1,25 +1,25 @@
-class Solution {
-    public int[] findThePrefixCommonArray(int[] A, int[] B) {
-        int n = A.length;
+// class Solution {
+//     public int[] findThePrefixCommonArray(int[] A, int[] B) {
+//         int n = A.length;
 
-        int[] result = new int[n];
+//         int[] result = new int[n];
 
-        for(int i = 0; i < n; i++) {
-            int count = 0;
+//         for(int i = 0; i < n; i++) {
+//             int count = 0;
 
-            for(int x = 0; x <= i; x++) {
-                for(int y = 0; y <= i; y++) {
-                    if(B[y] == A[x]) {
-                        count++;
-                        break;
-                    }
-                }
-            }
+//             for(int x = 0; x <= i; x++) {
+//                 for(int y = 0; y <= i; y++) {
+//                     if(B[y] == A[x]) {
+//                         count++;
+//                         break;
+//                     }
+//                 }
+//             }
 
-            result[i] = count;
-        }
+//             result[i] = count;
+//         }
 
-        return result;
+//         return result;
 
-    }
-}
+//     }
+// }
