7d27922674fba0a9c913a774b93530315c34a352
Honey sharma <114321260+honey9876@users.noreply.github.com>
2025-01-28 22:13:38 +0530

    Sort an Array

---
+++ placementQuestion/sortArrays.java
@@ -0,0 +1,11 @@
+package placementQuestion;
+
+import java.util.Arrays;
+
+public class sortArrays {
+    public static void main(String[] args) {
+        int [] arr = {3, 5, 2, 9, 8, 54 };
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
