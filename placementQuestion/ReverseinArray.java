8637cbe327cc88507d58f63171f94a9c0be88eac
Honey sharma <114321260+honey9876@users.noreply.github.com>
2025-01-28 21:34:33 +0530

    Reverse an Array, q2

---
+++ placementQuestion/ReverseinArray.java
@@ -0,0 +1,21 @@
+package placementQuestion;
+
+import java.util.Arrays;
+
+public class ReverseinArray {
+    public static void main(String[] args) {
+        int [] arr = {1, 2, 3, 4, 5};
+        int start = 0;
+        int end = arr.length -1;
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+    
+}
