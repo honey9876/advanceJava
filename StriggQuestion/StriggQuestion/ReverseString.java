536bbbe48c494ec870965c4c8722d08d2240c5ef
Honey sharma <114321260+honey9876@users.noreply.github.com>
2025-01-31 23:14:23 +0530

    Reverse String ,Q1

---
+++ StriggQuestion/StriggQuestion/ReverseString.java
@@ -0,0 +1,13 @@
+package StriggQuestion;
+
+public class ReverseString {
+    public static String Reverse(String str){
+        StringBuilder sb = new StringBuilder(str);
+        return sb.reverse().toString();
+    }
+    public static void main(String[] args) {
+        System.out.println(Reverse("hello sir I am #$#2"));
+    }
+
+    
+}
